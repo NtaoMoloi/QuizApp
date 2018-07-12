58d2d7c2f05574234bd35158a0febd95c1edf554
Ntaoleng Moloi <n_moloi@outlook.com>
2018-07-12 10:13:31 +0200

    modified code

--- app/src/main/java/com/example/android/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/android/quizapp/QuizActivity.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 public class QuizActivity extends AppCompatActivity {
 
     int score = 0;
+    String userAnswer;
     Intent initiateIntent;
 
     @Override
@@ -23,17 +24,17 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
         initiateIntent = getIntent();
 
-        TextView textView = (TextView) findViewById(R.id.quiz_title);
+        TextView textView = findViewById(R.id.quiz_title);
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Hometown.ttf");
         textView.setTypeface(typeface);
     }
 
     /* This method checks if the user checked the correct answers for question one,
-     * if correct the score add a point */
+     * if correct the score adds a point */
 
     private void questionOneRadioCheck() {
-        RadioButton tRadioButton = (RadioButton) findViewById(R.id.radio_true);
-        RadioButton fRadioButton = (RadioButton) findViewById(R.id.radio_false);
+        RadioButton tRadioButton = findViewById(R.id.radio_true);
+        RadioButton fRadioButton = findViewById(R.id.radio_false);
         boolean checkedTrue = tRadioButton.isChecked();
         boolean checkedFalse = fRadioButton.isChecked();
         if (checkedTrue) {
@@ -44,13 +45,13 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     /* This method checks if the user checked the correct answers for question two,
-     * if correct the score add a point */
+     * if correct the score adds a point */
 
 private void questionTwoCheck() {
-        CheckBox twoCheckBoxOne = (CheckBox) findViewById(R.id.question_two_check_one);
-        CheckBox twoCheckBoxTwo = (CheckBox) findViewById(R.id.question_two_check_two);
-        CheckBox twoCheckBoxThree = (CheckBox) findViewById(R.id.question_two_check_three);
-        CheckBox twoCheckBoxFour = (CheckBox) findViewById(R.id.question_two_check_four);
+        CheckBox twoCheckBoxOne = findViewById(R.id.question_two_check_one);
+        CheckBox twoCheckBoxTwo = findViewById(R.id.question_two_check_two);
+        CheckBox twoCheckBoxThree = findViewById(R.id.question_two_check_three);
+        CheckBox twoCheckBoxFour = findViewById(R.id.question_two_check_four);
         if (!twoCheckBoxOne.isChecked() && twoCheckBoxThree.isChecked() && twoCheckBoxFour.isChecked()) {
             score += 0;
         } else if (twoCheckBoxTwo.isChecked()) {
@@ -61,13 +62,13 @@ private void questionTwoCheck() {
     }
 
     /* This method checks if the user checked the correct answers for question three,
-     * if correct the score add a point */
+     * if correct the score adds a point */
 
     private void questionThreeRadioCheck() {
-        RadioButton q3rd1 = (RadioButton) findViewById(R.id.question_three_radio_one);
-        RadioButton q3r2 = (RadioButton) findViewById(R.id.question_three_radio_two);
-        RadioButton q3r3 = (RadioButton) findViewById(R.id.question_three_radio_three);
-        RadioButton q3r4 = (RadioButton) findViewById(R.id.question_three_radio_four);
+        RadioButton q3rd1 = findViewById(R.id.question_three_radio_one);
+        RadioButton q3r2 = findViewById(R.id.question_three_radio_two);
+        RadioButton q3r3 = findViewById(R.id.question_three_radio_three);
+        RadioButton q3r4 = findViewById(R.id.question_three_radio_four);
         boolean checkedOne = q3rd1.isChecked();
         boolean checkedTwo = q3r2.isChecked();
         boolean checkedThree = q3r3.isChecked();
@@ -81,12 +82,12 @@ private void questionTwoCheck() {
     }
 
     /* This method checks if the user checked the correct answers for question four,
-     * if correct the score add a point */
+     * if correct the score adds a point */
 
     private void questionFourRadioCheck() {
-        RadioButton q4rd1 = (RadioButton) findViewById(R.id.question_four_radio_one);
-        RadioButton q4rd2 = (RadioButton) findViewById(R.id.question_four_radio_two);
-        RadioButton q4rd3 = (RadioButton) findViewById(R.id.question_four_radio_three);
+        RadioButton q4rd1 = findViewById(R.id.question_four_radio_one);
+        RadioButton q4rd2 = findViewById(R.id.question_four_radio_two);
+        RadioButton q4rd3 = findViewById(R.id.question_four_radio_three);
         boolean checkedOne = q4rd1.isChecked();
         boolean checkedTwo = q4rd2.isChecked();
         boolean checkedThree = q4rd3.isChecked();
@@ -99,11 +100,12 @@ private void questionTwoCheck() {
     }
 
     /* This method checks if the user checked the correct answers for question five,
-     * if correct the score add a point */
+     * if correct the score adds a point */
 
     private void questionFiveEditText() {
-        EditText inputText = (EditText) findViewById(R.id.input_answer);
-        if (inputText.getText().toString().equals(getString(R.string.questionFiveAnswer))) {
+        EditText inputText = findViewById(R.id.input_answer);
+        userAnswer = inputText.getText().toString();
+        if (userAnswer.equalsIgnoreCase("granite")) {
             score += 1;
         } else {
             score += 0;
@@ -111,12 +113,12 @@ private void questionTwoCheck() {
     }
 
     /* This method checks if the user checked the correct answers for question six,
-     * if correct the score add a point */
+     * if correct the score adds a point */
 
     private void questionSixRadioCheck() {
-        RadioButton q6rd1 = (RadioButton) findViewById(R.id.question_six_radio_one);
-        RadioButton q6rd2 = (RadioButton) findViewById(R.id.question_six_radio_two);
-        RadioButton q6rd3 = (RadioButton) findViewById(R.id.question_six_radio_three);
+        RadioButton q6rd1 = findViewById(R.id.question_six_radio_one);
+        RadioButton q6rd2 = findViewById(R.id.question_six_radio_two);
+        RadioButton q6rd3 = findViewById(R.id.question_six_radio_three);
         boolean checkedOne = q6rd1.isChecked();
         boolean checkedTwo = q6rd2.isChecked();
         boolean checkedThree = q6rd3.isChecked();
@@ -129,11 +131,11 @@ private void questionTwoCheck() {
     }
 
     /* This method checks if the user checked the correct answers for question seven,
-     * if correct the score add a point */
+     * if correct the score adds a point */
 
     private void questionSevenRadioCheck() {
-        RadioButton RadioButtonTrue = (RadioButton) findViewById(R.id.seven_radio_true);
-        RadioButton RadioButtonFalse = (RadioButton) findViewById(R.id.seven_radio_false);
+        RadioButton RadioButtonTrue = findViewById(R.id.seven_radio_true);
+        RadioButton RadioButtonFalse = findViewById(R.id.seven_radio_false);
         boolean checkedTrue = RadioButtonTrue.isChecked();
         boolean checkedFalse = RadioButtonFalse.isChecked();
         if (checkedTrue) {
@@ -144,13 +146,13 @@ private void questionTwoCheck() {
     }
 
     /* This method checks if the user checked the correct answers for question eight,
-     * if correct the score add a point */
+     * if correct the score adds a point */
 
     private void questionEightCheck() {
-        CheckBox eightCheckBoxOne = (CheckBox) findViewById(R.id.question_eight_check_one);
-        CheckBox eightCheckBoxTwo = (CheckBox) findViewById(R.id.question_eight_check_two);
-        CheckBox eightCheckBoxThree = (CheckBox) findViewById(R.id.question_eight_check_three);
-        CheckBox eightCheckBoxFour = (CheckBox) findViewById(R.id.question_eight_check_four);
+        CheckBox eightCheckBoxOne = findViewById(R.id.question_eight_check_one);
+        CheckBox eightCheckBoxTwo = findViewById(R.id.question_eight_check_two);
+        CheckBox eightCheckBoxThree = findViewById(R.id.question_eight_check_three);
+        CheckBox eightCheckBoxFour = findViewById(R.id.question_eight_check_four);
         if (eightCheckBoxThree.isChecked() && eightCheckBoxFour.isChecked()) {
             score += 1;
         }else if (eightCheckBoxOne.isChecked() || eightCheckBoxTwo.isChecked()) {
@@ -159,12 +161,12 @@ private void questionTwoCheck() {
     }
 
     /* This method checks if the user checked the correct answers for question nine,
-     * if correct the score add a point */
+     * if correct the score adds a point */
 
     private void questionNineRadioCheck() {
-        RadioButton q9rd1 = (RadioButton) findViewById(R.id.question_nine_radio_one);
-        RadioButton q9rd2 = (RadioButton) findViewById(R.id.question_nine_radio_two);
-        RadioButton q9rd3 = (RadioButton) findViewById(R.id.question_nine_radio_three);
+        RadioButton q9rd1 = findViewById(R.id.question_nine_radio_one);
+        RadioButton q9rd2 = findViewById(R.id.question_nine_radio_two);
+        RadioButton q9rd3 = findViewById(R.id.question_nine_radio_three);
         boolean checkedOne = q9rd1.isChecked();
         boolean checkedTwo = q9rd2.isChecked();
         boolean checkedThree = q9rd3.isChecked();
@@ -177,12 +179,12 @@ private void questionTwoCheck() {
     }
 
     /* This method checks if the user checked the correct answers for question ten,
-     * if correct the score add a point */
+     * if correct the score adds a point */
 
     private void questionTenRadioCheck() {
-        RadioButton q10rd1 = (RadioButton) findViewById(R.id.question_ten_radio_one);
-        RadioButton q10rd2 = (RadioButton) findViewById(R.id.question_ten_radio_two);
-        RadioButton q10rd3 = (RadioButton) findViewById(R.id.question_ten_radio_three);
+        RadioButton q10rd1 = findViewById(R.id.question_ten_radio_one);
+        RadioButton q10rd2 = findViewById(R.id.question_ten_radio_two);
+        RadioButton q10rd3 = findViewById(R.id.question_ten_radio_three);
         boolean checkedOne = q10rd1.isChecked();
         boolean checkedTwo = q10rd2.isChecked();
         boolean checkedThree = q10rd3.isChecked();
@@ -194,7 +196,7 @@ private void questionTwoCheck() {
         }
     }
 
-    /* This method add all the scores for the previous questions */
+    /* This method adds all the scores for the previous questions */
 
     private void addScores() {
         score = 0;
